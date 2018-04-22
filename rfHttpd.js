dfb09fa5844a259994769137f0285b9ca4b4b47f
Li Chuan <lichuan_172@163.com>
2018-04-22 20:47:20 +0800

    Creat rfHttpd.js

---
+++ rfHttpd.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function(req, res){
+    if(req.url != '/favicon.ico'){
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        fs.readFile('./test/test.html', function(err, data){
+            if(err){
+                res.write(err.toString());
+            }else{
+                res.write(data.toString());
+            }
+            res.end();
+        });
+    }
+}).listen(80);
