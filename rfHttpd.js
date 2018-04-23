efbfb6d863247f0a6757afc8251c8dff258143fd
Li Chuan <lichuan_172@163.com>
2018-04-23 15:57:28 +0800

    try node's url module

--- rfHttpd.js
+++ rfHttpd.js
@@ -1,15 +1,18 @@
 var http = require('http');
-var fs = require('fs');
+//var fs = require('fs');
+var url = require('url');
 http.createServer(function(req, res){
     if(req.url != '/favicon.ico'){
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        fs.readFile('../www/index.html', function(err, data){
-            if(err){
-                res.write(err.toString());
-            }else{
-                res.write(data.toString());
-            }
-            res.end();
-        });
+//        fs.readFile('../www/index.html', function(err, data){
+//            if(err){
+//                res.write(err.toString());
+//            }else{
+//                res.write(data.toString());
+//            }
+//            res.end();
+//        });
+        var pathname = url.parse(res.url).pathname;
+        res.write(pathname);
     }
 }).listen(80);
